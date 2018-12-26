81a50579586be69443f0e81562c97516b4534ca3
MatthewDodi <matthew.dodi@gmail.com>
2018-12-25 19:01:09 -0800

    type: basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// to be continued...
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const faker = require('faker');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.send(faker.name.findName());
+});
+
+app.listen(3000, () => console.log('Server is now running on port 3000'));
