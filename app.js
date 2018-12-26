4148c080cc403d9aaead5e44c096b8d482633b5e
MatthewDodi <matthew.dodi@gmail.com>
2018-12-25 19:23:59 -0800

    type: feat - Added basic styling - Added pug (jade) templating engine

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 const faker = require('faker');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res, next) => {
-  res.send(faker.name.findName());
+app.get('/', (req, res) => {
+  res.render('index', { test: 'Working bruh' });
 });
 
 app.listen(3000, () => console.log('Server is now running on port 3000'));
